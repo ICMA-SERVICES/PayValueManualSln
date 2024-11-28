c28415df0bdc2e37d8b73e8b35bbdafabb5a8d3d
KodeTemplar <sirvictoregilla@gmail.com>
2024-11-28 10:46:54 +0100

    Made swagger to work in Release

--- PayValueManualSln.Api/Program.cs
+++ PayValueManualSln.Api/Program.cs
@@ -39,11 +39,11 @@ namespace PayValueManualSln.Api
 			builder.Services.AddSwaggerGen();
 			var app = builder.Build();
 			// Configure the HTTP request pipeline.
-			if (app.Environment.IsDevelopment())
-			{
+			//if (app.Environment.IsDevelopment())
+			//{
 				app.UseSwagger();
 				app.UseSwaggerUI();
-			}
+			//}
 
 			app.UseHttpsRedirection();
 
